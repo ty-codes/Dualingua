697447ae9c413a9fc1c8b013d18b678077e31e53
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-29 07:11:42 -0800

    Add files via upload

---
+++ src/assets/svg/index.js
@@ -0,0 +1,22 @@
+import {ReactComponent as Book} from "./book.svg";
+import {ReactComponent as Tablet} from "./tablet.svg";
+import {ReactComponent as Speaker} from "./speaker.svg";
+import {ReactComponent as Headset} from "./headset.svg";
+import {ReactComponent as Chart} from "./graph.svg";
+import {ReactComponent as Users} from "./users.svg";
+import {ReactComponent as Home} from "./home.svg";
+import {ReactComponent as Ellipsis} from "./ellipsis.svg";
+import {ReactComponent as Dot} from "./dot.svg";
+
+
+export {
+    Book,
+    Tablet,
+    Speaker,
+    Headset,
+    Chart, 
+    Users,
+    Home,
+    Ellipsis,
+    Dot
+}
