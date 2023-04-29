b00b7dd5d7fc69692de9f653b8a6f7ea30689c52
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-29 07:05:35 -0800

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import flame from "./flame.png";
+import female from "./female.jpeg";
+import male from "./male.jpeg";
+import britain from "./britain.png";
+import vocab from "./vocab.png";
+
+export {
+    flame,
+    male,
+    female,
+    britain, 
+    vocab
+};
