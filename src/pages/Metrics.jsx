e47ca431dd9128955d30e0792d02cf55fa3237f0
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-29 06:59:15 -0800

    Add files via upload

---
+++ src/pages/Metrics.jsx
@@ -0,0 +1,10 @@
+import { Calendar, PageWrapper, Skills } from "../components";
+
+export default function Metrics() {
+   return (
+      <PageWrapper>
+         <Calendar /><br />
+         <Skills />
+      </PageWrapper>
+   )
+}
