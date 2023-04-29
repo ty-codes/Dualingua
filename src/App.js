cffadb7698af8d8a28488c17bd21d343de7afd15
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-29 07:13:06 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Footer, Loader } from './components';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Metrics = React.lazy(() => import('./pages/Metrics'));
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='metrics' element={<Metrics />} />
+          </Routes>
+        </Suspense>
+            <Footer />
+  
+      </Router>
+    </>
+
+  );
+}
+
+export default App;
